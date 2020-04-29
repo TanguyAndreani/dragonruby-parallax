a6b52ffaef5e686bc2e5e297dfabdd2c1be36dc0
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-04-29 15:40:13 +0200

    added: left and right controls

--- app/main.rb
+++ app/main.rb
@@ -6,15 +6,36 @@ LAYERS = [
     'layer5.png',
 ].map { |s| "assets/#{s}" }
 
+def checkInput args
+    if args.inputs.keyboard.key_held.left
+        args.state.direction = 1
+    elsif args.inputs.keyboard.key_held.right
+        args.state.direction = -1
+    end
+end
+
 def tick args
+  args.state.direction ||= -1
+  args.state.xs ||= LAYERS.map { 0 }
+
   w = args.grid.right
   h = args.grid.top
 
-  LAYERS.each_with_index do |f, i|
-    i += 1
-    t = args.state.tick_count % (w / i)
-    x_coord = t * i * -1
-    args.outputs.sprites << [x_coord, 0, w, h, f]
-    args.outputs.sprites << [x_coord + (w - 2), 0, w, h, f]
+  relative_speed = 1.2 # relative speed of a layer
+  first_layer_move = 1 # or 0 for 'not moving'
+
+  checkInput args
+
+  LAYERS.each_with_index do |f, layer|
+    offset = relative_speed * (layer + first_layer_move)
+    offset *= args.state.direction
+
+    args.state.xs[layer] += offset
+    x_coord = args.state.xs[layer] % w
+    args.outputs.sprites << [
+      [x_coord - (w - 2), 0, w, h, f],
+      [x_coord, 0, w, h, f],
+      [x_coord + (w - 2), 0, w, h, f]
+    ]
   end
 end
