990579b87a917ff1126eac9916f9f2ba23d64f57
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-26 09:43:39 +0200

    fix easingfn class

--- lib/easing.rb
+++ lib/easing.rb
@@ -2,9 +2,10 @@ class EasingFn
   attr_reader :current
 
   def initialize from: 0, to: 10, time: 50
-    @from = from
-    @to = to
-    @time = time
+    @from = Float from
+    @to = Float to
+    @time = Float time
+
     @step = (to - from).abs / time
     @direction = (to > from) ? 1 : -1
     @current = from
@@ -12,8 +13,8 @@ class EasingFn
 
   def step! tick_count
     @first_tick ||= tick_count
-    diff = tick_count-@first_tick
-    if diff < @time
+    diff = tick_count-@first_tick+1
+    if diff <= @time
       @current = @from + @step*@direction*diff
     end
   end
@@ -23,3 +24,25 @@ class EasingFn
     @current = @from
   end
 end
+
+=begin
+a = EasingFn.new(from: 10.0, to: -4.0, time: 10.0)
+p a.current
+
+10.times do |tick|
+  a.step! tick
+  p a.current
+end
+
+10.0
+8.6
+7.2
+5.800000000000001
+4.4
+3.0
+1.6000000000000014
+0.20000000000000107
+-1.1999999999999993
+-2.5999999999999996
+-4.0
+=end
